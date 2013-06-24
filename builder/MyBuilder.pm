972397c3b0acf55f6cac4d74645eacf119f4c745
Syohei YOSHIDA <syohex@gmail.com>
2013-06-24 23:52:24 +0900

    fix for my own builder

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -3,6 +3,9 @@ use strict;
 use warnings;
 use 5.008001;
 use base 'Module::Build::XSUtil';
+use Devel::CheckLib;
+
+Devel::CheckLib::check_lib_or_exit(lib => ['aa'], header => ['aalib.h']);
 
 sub new {
     my ( $class, %args ) = @_;
@@ -14,6 +17,7 @@ sub new {
         },
         generate_ppport_h  => 'lib/Text/ppport.h',
         needs_compiler_c99 => 1,
+        extra_linker_flags => ['-laa'],
     );
     return $self;
 }
--- cpanfile
+++ cpanfile
@@ -3,8 +3,11 @@ requires 'XSLoader', '0.02';
 requires 'perl', '5.008001';
 
 on build => sub {
-    requires 'Devel::PPPort', '3.2';
-    requires 'ExtUtils::MakeMaker', '6.59';
-    requires 'ExtUtils::ParseXS', '2.21';
     requires 'Test::More', '0.88';
 };
+
+on configure => sub {
+    requires 'Module::Build::XSUtil', '>=0.02';
+    requires 'Devel::CheckLib';
+};
+
