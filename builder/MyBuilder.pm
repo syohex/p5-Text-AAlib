4c30dc6a463967fef4b6f883f8be540d4abadcf2
Syohei YOSHIDA <syohex@gmail.com>
2013-07-01 10:55:59 +0900

    Fix checking header file and library dependency #1

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -3,9 +3,33 @@ use strict;
 use warnings;
 use 5.008001;
 use base 'Module::Build::XSUtil';
+use File::Which;
 use Devel::CheckLib;
 
-Devel::CheckLib::check_lib_or_exit(lib => ['aa'], header => ['aalib.h']);
+which('aalib-config') or exit;
+my @link_flags = split ' ', `aalib-config --libs`;
+my @cflags     = split ' ', `aalib-config --cflags`;
+
+my (@libs, @lib_paths);
+for my $flag (@link_flags) {
+    if ($flag =~ m{\A-l(.+)\z}) {
+        push @libs, $1;
+    } elsif ($flag =~ m{\A-L(.+)\z}) {
+        push @lib_paths, $1;
+    }
+}
+
+my @inc_paths;
+for my $flag (@cflags) {
+    push @inc_paths, $1 if $flag =~ m{\A-I(.+)\z};
+}
+
+Devel::CheckLib::check_lib_or_exit(
+    lib     => [@libs],
+    libpath => [@lib_paths],
+    incpath => [@inc_paths],
+    header  => ['aalib.h'],
+);
 
 sub new {
     my ( $class, %args ) = @_;
@@ -17,10 +41,10 @@ sub new {
         },
         generate_ppport_h  => 'lib/Text/ppport.h',
         needs_compiler_c99 => 1,
-        extra_linker_flags => ['-laa'],
+        extra_compiler_flags => [ @cflags ],
+        extra_linker_flags   => [ @link_flags ],
     );
     return $self;
 }
 
 1;
-
